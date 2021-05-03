9350d3fda2c9fc984753505f06ed3ac48ce27755
Temirlan <Temirlan200312@gmai.com>
2021-05-03 14:14:46 +0600

    iterativepower.go

--- iterativepower.go
+++ iterativepower.go
@@ -7,14 +7,14 @@ func IterativePower(nb int, power int) int {
 	if power == 0 {
 		return 1
 	}
-	result := 0
+	result := nb
 	if power > 0 {
-		for i := 0; i <= power; i++ {
-			result += nb * nb
+		for i := 0; i < power-1; i++ {
+			result *= nb
 		}
 	} else {
-		for i := 0; i <= power; i++ {
-			result += 1 / (nb * nb)
+		for i := 0; i < power-1; i++ {
+			result *= 1 / (result * nb)
 		}
 	}
 	return result
