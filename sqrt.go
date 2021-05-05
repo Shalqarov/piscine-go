a2e145a67aeda21a10972ed62e8ad7bd3232cf3e
Temirlan <Temirlan200312@gmai.com>
2021-05-05 20:37:59 +0600

    sqrt.go

--- sqrt.go
+++ sqrt.go
@@ -7,12 +7,10 @@ func Sqrt(nb int) int {
 	if nb <= 3 {
 		return 0
 	}
-	for i := 1; i < nb/2; i++ {
+	for i := 1; i*i <= nb; i++ {
 		if i*i == nb {
 			return i
-		} else if i*i > nb {
-			return 0
 		}
 	}
-	return nb
+	return 0
 }
