8202a571712a5f0c4214256fa76d76198fad6fba
Temirlan <Temirlan200312@gmai.com>
2021-05-06 01:58:49 +0600

    alphacount.go

--- alphacount.go
+++ alphacount.go
@@ -2,7 +2,7 @@ package piscine
 
 func AlphaCount(s string) int {
 	counter := 0
-	for i := 0; i < len(s)-1; i++ {
+	for i := 0; i < len(s); i++ {
 		if s[i] >= 65 && s[i] <= 90 || s[i] >= 97 && s[i] <= 122 {
 			counter++
 		}
