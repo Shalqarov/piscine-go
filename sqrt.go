364b89e1697f814a1c75ac356a76911efe0a9893
Temirlan <Temirlan200312@gmai.com>
2021-05-05 20:29:25 +0600

    sqrt.go

--- sqrt.go
+++ sqrt.go
@@ -1,18 +1,12 @@
 package piscine
 
 func Sqrt(nb int) int {
-	temp := 0
-	num := nb
-	sqrt := num / 2
-	for sqrt != temp {
-		temp = sqrt
-		sqrt = (num/temp + temp) / 2
-		if sqrt == 1 || sqrt*sqrt < nb {
+	for i := 1; i < nb/2; i++ {
+		if i*i == nb {
+			return i
+		} else if i*i > nb {
 			return 0
 		}
 	}
-	if sqrt*sqrt != nb {
-		return 0
-	}
-	return sqrt
+	return nb
 }
