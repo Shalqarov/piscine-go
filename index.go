391e27ea4c741c87593b4ffa50f2875c666da5ce
Temirlan <Temirlan200312@gmai.com>
2021-05-06 16:48:11 +0600

    index.go

--- index.go
+++ index.go
@@ -1,22 +1,18 @@
 package piscine
 
 func Index(s string, toFind string) int {
-	if len(toFind) == 0 || len(s) < len(toFind) {
-		return -1
-	}
-	count := 0
-	for i := 0; i <= len(s)-len(toFind); i++ {
-		if s[i] == toFind[0] {
-			for j, k := 0, i; j < len(toFind); j, k = j+1, k+1 {
-				if s[k] != toFind[j] {
-					break
-				}
-				count++
+	for i := 0; i < len(s)-len(toFind); i++ {
+		for j := 0; s[i+j] == toFind[j]; j++ {
+			if j-len(toFind) == 1 && i == len(s)-len(toFind) {
+				return i
 			}
-			if count == len(toFind) {
+			if j == len(toFind) {
+
+			} else {
 				return i
 			}
 		}
+
 	}
 	return -1
 }
