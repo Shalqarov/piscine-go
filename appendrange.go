e5eeb4c75f7be7c86acd7d43b01117906f36a850
Temirlan <Temirlan200312@gmai.com>
2021-05-10 21:18:23 +0600

    appendrange.go

---
+++ appendrange.go
@@ -0,0 +1,13 @@
+package piscine
+
+func AppendRange(min, max int) []int {
+	var size int = max - min
+	if size < 1 {
+		return []int(nil)
+	}
+	mas := []int(nil)
+	for i := min; i < size+max; i++ {
+		mas = append(mas, i)
+	}
+	return mas
+}
