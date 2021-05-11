0217eed6da7d3f795072657a8013d8159f76ab15
Temirlan <Temirlan200312@gmai.com>
2021-05-11 21:13:54 +0600

    point

--- point/main.go
+++ point/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"github.com/01-edu/z01"
+)
 
 type point struct {
 	x int
@@ -12,8 +14,51 @@ func setPoint(ptr *point) {
 	ptr.y = 21
 }
 
+func returnInt(num int) int {
+	cnt := 0
+	if num < 0 {
+		z01.PrintRune('-')
+		num *= -1
+		cnt++
+	} else if num == 0 {
+		z01.PrintRune('0')
+		cnt++
+	}
+	var numbers []int32
+	for num != 0 {
+		d := num % 10
+		if d < 0 {
+			d *= -1
+			num *= -1
+		}
+		numbers = append(numbers, int32(d))
+		num /= 10
+		cnt++
+	}
+	for i := len(numbers) - 1; i >= 0; i-- {
+		z01.PrintRune(numbers[i] + 48)
+	}
+	return cnt
+}
+
+func print(pointers *point) {
+	z01.PrintRune('x')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	returnInt(pointers.x)
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	z01.PrintRune('y')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	returnInt(pointers.y)
+}
+
 func main() {
 	points := &point{}
 	setPoint(points)
-	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
+	print(points)
+	z01.PrintRune('\n')
 }
