4e4d0ba0611906264512ca0775e7de09baddb1b6
Temirlan <Temirlan200312@gmai.com>
2021-05-13 12:28:26 +0600

    comcheck

--- comcheck/main.go
+++ comcheck/main.go
@@ -7,10 +7,7 @@ import (
 
 func main() {
 	args := os.Args[1:]
-	if len(args) < 1 {
-		return
-	}
-	for i := 1; i < len(args); i++ {
+	for i := 0; i < len(args); i++ {
 		if args[i] == "01" || args[i] == "galaxy" || args[i] == "galaxy 01" {
 			fmt.Println("Alert!!!")
 			return
