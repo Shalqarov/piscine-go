7bd28ed13a5b8ee5f6d643d17b08ad799a75c61e
Temirlan <Temirlan200312@gmai.com>
2021-05-14 04:25:19 +0600

    unmatch.go

--- unmatch.go
+++ unmatch.go
@@ -3,14 +3,9 @@ package piscine
 func Unmatch(a []int) int {
 	for i := 0; i < len(a); i++ {
 		searching := false
-		cnt := 0
 		for j := 0; j < len(a); j++ {
 			if a[j] == a[i] && i != j {
 				searching = true
-				cnt++
-			}
-			if cnt == 2 {
-				return a[i]
 			}
 		}
 		if searching == false {
