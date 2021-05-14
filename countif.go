bbd64f5ecefdcecba2a739637b4693e6b4465ffe
Temirlan <Temirlan200312@gmai.com>
2021-05-15 03:08:24 +0600

    countif.go

--- countif.go
+++ countif.go
@@ -4,11 +4,8 @@ func CountIf(f func(string) bool, tab []string) int {
 	cnt := 0
 	for i := 0; i < len(tab); i++ {
 		if f(tab[i]) == true {
-			for j := 0; j < len(tab[i]); j++ {
-				cnt++
-			}
-			return cnt
+			cnt++
 		}
 	}
-	return 0
+	return cnt
 }
