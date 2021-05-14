46c9ca1395c85a4e8e5f488dd8c6462fbcc49fc1
Temirlan <Temirlan200312@gmai.com>
2021-05-15 03:16:33 +0600

    issorted.go

--- issorted.go
+++ issorted.go
@@ -3,7 +3,7 @@ package piscine
 func IsSorted(f func(a, b int) int, a []int) bool {
 	for i := 0; i < len(a); i++ {
 		for j := 0; j < len(a); j++ {
-			if f(a[j], a[j+1]) < 0 {
+			if f(a[j], a[j+1]) > 0 {
 				return false
 			}
 		}
