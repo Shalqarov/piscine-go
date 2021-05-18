98c943a5dd2339de35870c1f49b3827fc61714c4
Temirlan <Temirlan200312@gmai.com>
2021-05-18 22:05:18 +0600

    listpushfront.go

--- listpushfront.go
+++ listpushfront.go
@@ -4,8 +4,6 @@ func ListPushFront(l *List, data interface{}) {
 	if l.Head == nil {
 		l.Head = &NodeL{Data: data}
 	} else {
-		temp := l.Head
-		temp = temp.Next
-		temp.Next = &NodeL{Data: data}
+		l.Head = &NodeL{Data: data, Next: l.Head}
 	}
 }
