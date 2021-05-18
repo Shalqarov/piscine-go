21faf72de2cdb49e12aace726db7403b5bb49a33
Temirlan <Temirlan200312@gmai.com>
2021-05-19 02:24:53 +0600

    listfind.go

---
+++ listfind.go
@@ -0,0 +1,21 @@
+package piscine
+
+func CompStr(a, b interface{}) bool {
+	return a == b
+}
+
+func ListFind(l *List, ref interface{}, comp func(a, b interface{}) bool) *interface{} {
+	if l.Head == nil {
+		return nil
+	}
+
+	temp := l.Head
+	for temp != nil {
+		if comp(temp.Data, ref) {
+			return &temp.Data
+		}
+		temp = temp.Next
+	}
+
+	return nil
+}
