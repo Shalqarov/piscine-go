3cc39e5efb4d48cb6d1c8cf3507d824d69a60555
Temirlan <Temirlan200312@gmai.com>
2021-05-19 01:58:28 +0600

    listreverse.go

--- listreverse.go
+++ listreverse.go
@@ -4,15 +4,13 @@ func ListReverse(l *List) {
 	if l.Head == nil {
 		return
 	}
-	current := l.Head
-	l.Tail = &NodeL{Data: l.Head.Data, Next: nil}
-	prev := l.Tail
-	prev = nil
-	for current != nil {
-		l.Head.Next = current.Next
-		current.Next = prev
+	prev := &NodeL{Data: l.Head.Data, Next: nil}
+	for i := 0; i < ListSize(l); i++ {
+		current := l.Head
+		for current.Next != nil && current.Next != prev {
+			current.Data, current.Next.Data = current.Next.Data, current.Data
+			current = current.Next
+		}
 		prev = current
-		current = current.Next
 	}
-	l.Head = prev
 }
