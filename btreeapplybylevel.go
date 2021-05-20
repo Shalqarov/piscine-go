7a28773b3f1296d118037c5bdc53ead1bc4da4f5
Temirlan <Temirlan200312@gmai.com>
2021-05-21 03:12:35 +0600

    1

--- btreeapplybylevel.go
+++ btreeapplybylevel.go
@@ -1,10 +1,20 @@
 package piscine
 
 func BTreeApplyByLevel(root *TreeNode, f func(...interface{}) (int, error)) {
+	size := BTreeLevelCount(root)
+	for i := 1; i <= size; i++ {
+		BtreeCurrentLevel(root, size, f)
+	}
+}
+
+func BtreeCurrentLevel(root *TreeNode, size int, f func(...interface{}) (int, error)) {
 	if root == nil {
 		return
 	}
-	f(root.Data)
-	BTreeApplyByLevel(root.Left, f)
-	BTreeApplyByLevel(root.Right, f)
+	if size == 1 {
+		f(root.Data)
+	} else if size > 1 {
+		BtreeCurrentLevel(root.Left, size-1, f)
+		BtreeCurrentLevel(root.Right, size-1, f)
+	}
 }
